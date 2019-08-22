79eed86d4514684ca9af104cb22b410e6c721d7f
snehG0205 <sneh.gajiwala@gmail.com>
2019-08-22 11:59:25 -0500

    started with the refresher course

---
+++ Hello_World.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 11:32:14 2019
+
+@author: snehgajiwala
+"""
+
+import sys
+
+def main():
+    print ("Hello World!")
+    
+    
+    
+if __name__ == '__main__':
+    main()
---
+++ UserDefinedFunction.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 11:53:02 2019
+
+@author: snehgajiwala
+"""
+def printNow(s):
+    print("Hello, "+s+"!")
+    
+
+def main():
+    printNow("Sneh")
+    
+    
+if __name__ == '__main__':
+    main()
